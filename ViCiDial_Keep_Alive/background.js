7443befabbdef5444a5ca17ae02fb4643c52b8f4
TheBlode <theblode2020@gmail.com>
2021-03-26 08:50:32 +0000

    - adding quiet.mp3. - adding autoplay looping sound back in.
    - adding quiet.mp3.
    - adding autoplay looping sound back in.

--- ViCiDial_Keep_Alive/background.js
+++ ViCiDial_Keep_Alive/background.js
@@ -5,11 +5,5 @@ setTimeout(function() {
     $div.attr("id", "sound_area");
 
     // Append audio to the sound_area div
-    $("#sound_area").append("<audio id=\"sound\" autoplay loop><source src=\"https://download1491.mediafire.com/9iq03yt56v3g/say66szyc242e44/quiet.mp3\"></audio>");
-
-    // Get audio element
-    var my_sound = document.getElementById("sound");
-
-    // Play sound
-    my_sound.play();
-}, 30000);
+    $("#sound_area").append("<audio id=\"sound\" autoplay loop><source src=\"/agc/sounds/quiet.mp3\"></audio>");
+}, 20000);
